3689c87f8584def3021242d96ae53873d44aa1d3
DARMAGRANDPRIX <163404531+DARMAGRANDPRIX@users.noreply.github.com>
2024-05-17 12:37:31 +0900

    Add files via upload

--- Review/repository/ReviewRepository.java
+++ Review/repository/ReviewRepository.java
@@ -1,3 +1,5 @@
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
     Page<Review> findByHouseId(Integer houseId, Pageable pageable);
+    Optical<Review> findByHouseIdAndUserId(Integer houseId, Integer userId);
+    List<Review> findTop6ByHouseIdOrderByCreatedAtDesc(Integer houseId);
 }
